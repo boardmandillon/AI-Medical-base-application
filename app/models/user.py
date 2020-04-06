e58d463f55f04865eb985b40d9c28dd8f635229b
Miles Spencer <sc16mls@leeds.ac.uk>
2020-04-06 17:51:28 +0100

    Added last login to user model and made new database migration

--- app/models/user.py
+++ app/models/user.py
@@ -36,6 +36,7 @@ class User(UserMixin, db.Model):
     date_of_birth = db.Column(db.Date, index=True)
     date_registered = db.Column(db.DateTime, index=True,
                                 default=datetime.utcnow().replace(microsecond=0))
+    last_login = db.Column(db.DateTime, index=True)
     password_hash = db.Column(db.String(128))
     token = db.Column(db.String(32), index=True, unique=True)
     token_expiration = db.Column(db.DateTime)
@@ -85,8 +86,12 @@ class User(UserMixin, db.Model):
             self.set_password(data['password'])
 
     def get_token(self, expires_in=3600):
-        """Returns a randomly string token to the user."""
+        """Sets the last login and returns a random string token to the user.
+
+        :param expires_in: Time until the token expires in seconds
+        """
         now = datetime.utcnow()
+        self.last_login = now.replace(microsecond=0)
         if self.token and self.token_expiration > now + timedelta(seconds=60):
             return self.token
         self.token = base64.b64encode(os.urandom(24)).decode('utf-8')
