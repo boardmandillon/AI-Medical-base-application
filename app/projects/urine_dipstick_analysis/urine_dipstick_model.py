3181938727e480be744a2af16a2f0f25ab08c938
Miles Spencer <sc16mls@leeds.ac.uk>
2020-04-09 18:14:00 +0100

    Created a model for urine dipstick project for uploading photos

---
+++ app/projects/urine_dipstick_analysis/urine_dipstick_model.py
@@ -0,0 +1,20 @@
+from app.models.project_base import ProjectBase
+from app import db_mongo as db
+
+from datetime import datetime
+
+
+class UrineDipstickModel(ProjectBase):
+    """Document definition for the urine dipstick analysis project.
+    Stores the image metadata including filename, upload date and content type.
+    Image is stored in binary format using GridFS to divide the file into chunks"""
+
+    possible_labels = {}
+
+    filename = db.StringField()
+    upload_date = db.DateTimeField(default=datetime.utcnow())
+    content_type = db.StringField()
+    diagnosis_photo = db.FileField()
+
+    t_diagnosis = db.ListField(db.StringField())
+    l_actual_diagnosis = db.StringField()
