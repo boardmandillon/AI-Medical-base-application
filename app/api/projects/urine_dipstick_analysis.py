e12d9fb1ee6e42db4362c0faadef677879aceb64
Miles Spencer <sc16mls@leeds.ac.uk>
2020-04-16 15:14:34 +0100

    Restructured project files and caught errors when unable to detect urine dipstick

--- app/api/projects/urine_dipstick_analysis.py
+++ app/api/projects/urine_dipstick_analysis.py
@@ -1,20 +1,17 @@
-from flask import request, g
-from flask import jsonify
+from flask import request, g, jsonify
 
 from app.api import bp
 from app.api.auth import token_auth
 from app.projects.urine_dipstick_analysis.urine_dipstick_model import \
     UrineDipstickModel
+from app.projects.urine_dipstick_analysis.urine_dipstick_image_pre_processing import \
+    image_pre_processing
 from app.api.errors import bad_request
 from PIL import Image
 import io
 import base64
 import binascii
 import bson
-import numpy as np
-from cv2 import cv2 as cv
-import imutils
-import os
 
 
 @bp.route('/urine_dipstick_analysis/', methods=['POST'])
@@ -81,103 +78,3 @@ def get_urine_analysis():
 
     data = {'message': 'image retrieved successfully'}
     return jsonify(data), 200
-
-
-def image_pre_processing(diagnosis_photo):
-    np_array = np.frombuffer(diagnosis_photo, dtype=np.uint8)
-    image = cv.imdecode(np_array, flags=1)
-
-    contours = get_image_contours(image)
-    dipstick_contour = get_dipstick_contour(contours)
-    dipstick = retrieve_dipstick_image(image, dipstick_contour)
-    dipstick_squares = get_dipstick_squares(dipstick)
-
-    print("Shape: {}".format(dipstick.shape))
-
-    # image = cv.drawContours(image, [dipstick_contour], -1, (0, 255, 0), 3)
-
-    out_filepath = '/Users/Miles/Desktop/Squares/'
-    cv.imwrite(os.path.join(out_filepath, 'dipstick.jpg'), dipstick)
-    for index, square in enumerate(dipstick_squares):
-        cv.imwrite(os.path.join(out_filepath, 'square' + str(index) + '.jpg'), square)
-
-
-def get_image_contours(image):
-    """ Extracts contours from the image.
-
-    Image is converted to a grayscale and a bilateral filter is applied to reduce noise
-    and preserve edges. Then a canny filter is used to catch strong edges, contours are
-    grabbed and the ten largest contours are retrieved based on area.
-    """
-    image_gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-    image_filtered = cv.bilateralFilter(image_gray, 15, 75, 75)
-    image_edges = cv.Canny(image_filtered, 30, 200)
-
-    contours = cv.findContours(image_edges.copy(), cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-    contours = imutils.grab_contours(contours)
-    contours = sorted(contours, key=cv.contourArea, reverse=True)[:10]
-    return contours
-
-
-def get_dipstick_contour(contours):
-    """ Searches contours for contour with four vertex points, rectangle/square shape.
-    When a rectangle/square is detected compute the bounding box and aspect ratio to
-    confirm its the rectangle dipstick.
-    """
-    dipstick_contour = None
-    for contour in contours:
-        perimeter = cv.arcLength(contour, True)
-        approx = cv.approxPolyDP(contour, 0.015 * perimeter, True)
-
-        if len(approx) == 4:
-            (x, y, width, height) = cv.boundingRect(approx)
-            aspect_ratio = float(width) / height
-
-            if aspect_ratio > 1.05:
-                dipstick_contour = contour
-                break
-
-    return dipstick_contour
-
-
-def retrieve_dipstick_image(image, contour):
-    """ Calculate the minimum area bounding the dipstick image, then apply a
-    rotation matrix to straighten the rectangle. If the dipstick has been rotated
-    into a vertical position it is rotated anticlockwise. The horizontal dipstick
-    image is then resized for extracting squares on the dipstick.
-    """
-    rectangle = cv.minAreaRect(contour)
-    center, size, theta = rectangle
-    height, width = image.shape[:2]
-
-    center, size = tuple(map(int, center)), tuple(map(int, size))
-    matrix = cv.getRotationMatrix2D(center, theta, 1)
-    dst = cv.warpAffine(image, matrix, (width, height))
-    dipstick = cv.getRectSubPix(dst, size, center)
-
-    if dipstick.shape[0] > dipstick.shape[1]:
-        dipstick = cv.rotate(dipstick, cv.ROTATE_90_COUNTERCLOCKWISE)
-
-    dipstick = imutils.resize(dipstick, width=800)
-    return dipstick
-
-
-def get_dipstick_squares(dipstick):
-    """ Slices the dipstick image cropping out the ten squares on the dipstick
-    based on their position and saving them to a list.
-    """
-    dipstick_squares = []
-
-    square_start = 15
-    square_end = 50
-    square_size = 35
-    distance_between_squares = 20
-    next_square = (square_size + distance_between_squares)
-    for x in range(10):
-        square = dipstick[0:dipstick.shape[0], square_start:square_end]
-        dipstick_squares.append(square)
-
-        square_start += next_square
-        square_end += next_square
-
-    return dipstick_squares
