2b3570e4a4fcbcaa2a198b862c8dd1aa63addf3b
nollid <ll16d4b@leeds.ac.uk>
2021-01-11 18:40:31 +0000

    Switch to JWT authentication with `flask-jwt-extended`
    Add refresh token api

--- app/api/projects/pointofcareOCR.py
+++ app/api/projects/pointofcareOCR.py
@@ -1,12 +1,12 @@
 from flask import request, g, jsonify
+from flask_jwt_extended import (jwt_required, get_jwt_identity)
 
 from app.api import bp
-from app.api.auth import token_auth
 from app.projects.pointofcare_ocr.pointofcare_model import POC_OCR_Model
 
 
 @bp.route('/pocresult', methods=['POST'])
-@token_auth.login_required
+@jwt_required
 def pocResult():
     """Creates record of Blood pressure results from JSON data in the request."""
     if request.headers['Content-Type'] == 'application/json':
@@ -19,25 +19,33 @@ def pocResult():
     diastolic = data.get('diastolic')
     heartRate = data.get('heartRate')
 
-    model = POC_OCR_Model(user_id=g.current_user.id, time=time, systolic=systolic, diastolic=diastolic,
-                          heartRate=heartRate)
-    model.save()
+    current_user = get_jwt_identity()
 
+    model = POC_OCR_Model(
+        user_id=current_user.id,
+        time=time,
+        systolic=systolic,
+        diastolic=diastolic,
+        heartRate=heartRate
+    )
+
+    model.save()
     return jsonify(model), 201
 
 
 @bp.route('/pocresult')
-@token_auth.login_required
+@jwt_required
 def getPocRecords():
     """Retrieves records of a user."""
-    return jsonify(POC_OCR_Model.objects().filter(
-        user_id=g.current_user.id))
+    current_user = get_jwt_identity()
+    return jsonify(POC_OCR_Model.objects().filter(user_id=current_user.id))
 
 
 @bp.route('/pocresult/<doc_id>', methods=['DELETE'])
-@token_auth.login_required
+@jwt_required
 def poc_delete_from_id(doc_id):
     """Deletes records corresponding to the given ID."""
-    POC_OCR_Model.objects.get_or_404(
-        id=doc_id, user_id=g.current_user.id).delete()
+    current_user = get_jwt_identity()
+    POC_OCR_Model.objects.get_or_404(id=doc_id, user_id=current_user.id).delete()
+
     return jsonify({"success": True})
