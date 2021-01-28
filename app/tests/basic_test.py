bb4d5deaf51afc79c2127601382109fa2e5ab6ce
Gabbie <sc17gs@leeds.ac.uk>
2021-01-28 18:56:43 +0000

    added unit tests for create_user and password_reset in users.py, started password mocking reset password token

--- app/tests/basic_test.py
+++ app/tests/basic_test.py
@@ -1,10 +1,12 @@
 import os
 import unittest
+import json
 
 from app import create_app, db_relational as db
 from setup import setUp
 from config import Config, basedir
-
+from unittest.mock import MagicMock, patch
+from app.models.user import User
 
 class TestConfig(Config):
     TESTING = True
@@ -12,6 +14,7 @@ class TestConfig(Config):
 
 class BasicTest(unittest.TestCase):
     """Class for basic test cases."""
+    
     def setUp(self):
         "set up test fixtures"
         app = create_app(TestConfig)
@@ -31,15 +34,20 @@ class BasicTest(unittest.TestCase):
 
     # Unit test cases
     ############################################################################
-    def test_valid_user_registration(self):
+    def test_create_user_returns201_andCorrectUserData_whenValid(self):
         response = self.register(
             'user@email.com',
             'password',
             'Test McTest'
         )
-        self.assertEqual(response.status_code, 201)
 
-    def test_duplicate_user_registration(self):
+        response_data = json.loads(response.get_data().decode("utf-8"))
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response_data['email'], 'user@email.com')
+        self.assertEqual(response_data['id'], 1)
+        self.assertEqual(response_data['name'], 'Test McTest')
+
+    def test_create_user_returns400AndErrorMessage_whenDuplicateEmailRegisters(self):
         setUp.setUpTestUser()
 
         response = self.register(
@@ -48,7 +56,87 @@ class BasicTest(unittest.TestCase):
             'Test McTest'
         )
 
+        response_message = json.loads(response.get_data().decode("utf-8"))['message']
         self.assertEqual(response.status_code, 400)
+        self.assertEqual(response_message,'Please use a different email address')
+
+    def test_create_user_returns400AndErrorMessage_whenMissingEmail(self):
+            response = self.register(
+                None,
+                'password',
+                'Test McTest'
+            )
+
+            response_message = json.loads(response.get_data().decode("utf-8"))['message']
+            self.assertEqual(response.status_code, 400)
+            self.assertEqual(response_message,"Must include 'email', 'password', 'name'")
+
+    def test_create_user_returns400AndErrorMessage_whenMissingName(self):
+            response = self.register(
+                'user@email.com',
+                'password',
+                None
+            )
+
+            response_message = json.loads(response.get_data().decode("utf-8"))['message']
+            self.assertEqual(response.status_code, 400)
+            self.assertEqual(response_message,"Must include 'email', 'password', 'name'")
+
+    def test_create_user_returns400AndErrorMessage_whenMissingPassword(self):
+            response = self.register(
+                'user@email.com',
+                None,
+                'Test McTest'
+            )
+
+            response_message = json.loads(response.get_data().decode("utf-8"))['message']
+            self.assertEqual(response.status_code, 400)
+            self.assertEqual(response_message,"Must include 'email', 'password', 'name'")
+
+    def test_password_reset_returns204_whenValidRequest(self):
+        response = self.reset_password_request(
+            'user@email.com'
+        )
+
+        self.assertEqual(response.status_code, 204)
+
+    def test_password_reset_returns400AndErrorMessage_whenMissingEmail(self):
+        response = self.reset_password_request(
+            None
+        )
+
+        response_message = json.loads(response.get_data().decode("utf-8"))['message']
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response_message,"Must include 'email' field")
+
+    # @patch('app.models.user.User.verify_reset_password_token', return_value='Test McTest')
+    # @patch('app.api.users.User.verify_reset_password_token', return_value='Test McTest')
+    @patch('app.api.users.User.verify_reset_password_token')
+    def test_password_returns204_whenValidNewPasswordRequest(self, test_patch):
+        # setUp.setUpTestUser()
+        test_patch.return_value('Test McTest')
+        
+        response = self.set_new_password(
+            'token',
+            'newPassword'
+        )
+
+        # response_message = json.loads(response.get_data().decode("utf-8"))['message']
+        # print("REPSONSE MESSAGE: ",response_message)
+
+        self.assertEqual(response.status_code, 204)
+
+
+    def test_password_returns403AndErrorMessage_wheninvalidPasswordResetToken(self):
+        response = self.set_new_password(
+            'invalidToken',
+            'newPassword'
+        )
+
+        response_message = json.loads(response.get_data().decode("utf-8"))['message']
+        self.assertEqual(response.status_code, 403)
+        self.assertEqual(response_message,"Invalid password reset token")
+
 
     def test_valid_user_login(self):
         setUp.setUpTestUser()
@@ -75,6 +163,18 @@ class BasicTest(unittest.TestCase):
         return self.app.test_client().post(
             '/api/users',
             data=dict(email=email, password=password, name=name)
+        )
+
+    def reset_password_request(self, email):
+        return self.app.test_client().put(
+            '/api/users/password-reset',
+            data=dict(email=email)
+        )
+    
+    def set_new_password(self, token, new_password):
+        return self.app.test_client().put(
+            '/api/users/password',
+            data=dict(token=token, new_password = new_password)
         )
 
     def login(self, email, password):
